aab70dd96bcc2b9ecc4af6442f1d81f641ce8dd8
Milco Numan <milco.numan@gmail.com>
2025-01-16 15:21:39 +0000

    Containerized environment for training

--- exercises/2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ exercises/2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -1,7 +1,8 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
-    // TODO
+    let max_value = input.iter().max().unwrap();
+    let min_value = input.iter().min().unwrap();
 
-    println!("{} is largest and {} is smallest");
+    println!("{} is largest and {} is smallest", max_value, min_value);
 }
